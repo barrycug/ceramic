a9dd1bf260e282466d81467bf6dd6c413c71fd31
Michael Daines <michael@mdaines.com>
2012-07-10 16:21:05 -0500

    slice array tags at lower zoom levels, don't use simplify option on point, adjust zoom conditions

--- basic.rb
+++ basic.rb
@@ -69,11 +69,11 @@ class Basic
     # Medium zoom sources
     
     medium_zoom_point_source = Cover::Source.new(
-      "(select osm_id, way, tags from planet_osm_point where place in ('city', 'town') or \"natural\" = 'peak') as q",
+      "(select osm_id, way, slice(tags, ARRAY['place', 'natural', 'name']) as tags from planet_osm_point where place in ('city', 'town') or \"natural\" = 'peak') as q",
       connection: @connection,
       srid: 900913,
       geometry: {
-        "way" => { type: :point, simplify: 16 }
+        "way" => { type: :point }
       },
       bbox: "way",
       convert: {
@@ -83,7 +83,7 @@ class Basic
     )
     
     medium_zoom_line_source = Cover::Source.new(
-      "(select osm_id, way, tags, ST_PointOnSurface(way) as point from planet_osm_line where (\"waterway\" = 'river' or highway in ('motorway', 'trunk', 'primary', 'secondary') or admin_level <> '' or route = 'ferry') and ST_Length(way) > :unit::float * 64) as q",
+      "(select osm_id, way, slice(tags, ARRAY['waterway', 'highway', 'admin_level', 'route', 'name']) as tags, ST_PointOnSurface(way) as point from planet_osm_line where (\"waterway\" = 'river' or highway in ('motorway', 'trunk', 'primary', 'secondary') or admin_level <> '' or route = 'ferry') and ST_Length(way) > :unit::float * 64) as q",
       connection: @connection,
       srid: 900913,
       geometry: {
@@ -98,7 +98,7 @@ class Basic
     )
     
     medium_zoom_polygon_source = Cover::Source.new(
-      "(select osm_id, way, tags, ST_PointOnSurface(ST_Buffer(way, 0)) as point from planet_osm_polygon where (\"natural\" in ('water', 'wood', 'land', 'beach', 'bay') or \"landuse\" in ('forest', 'residential') or waterway in ('lake', 'river') or boundary in ('administrative', 'protected_area', 'national_park')) and ST_Area(way) > :unit::float * :unit::float * 256 * 256) as q",
+      "(select osm_id, way, slice(tags, ARRAY['natural', 'landuse', 'waterway', 'boundary', 'name']) as tags, ST_PointOnSurface(ST_Buffer(way, 0)) as point from planet_osm_polygon where (\"natural\" in ('water', 'wood', 'land', 'beach', 'bay') or \"landuse\" in ('forest', 'residential') or waterway in ('lake', 'river') or boundary in ('administrative', 'protected_area', 'national_park')) and ST_Area(way) > :unit::float * :unit::float * 256 * 256) as q",
       connection: @connection,
       srid: 900913,
       geometry: {
@@ -164,14 +164,14 @@ class Basic
     
     Cover::Maker.new(scale: 8192) do |index, features|
       
-      if index.z > 13
+      if index.z >= 11
         
         features.make(high_zoom_coastline_source, coastline_builder)
         features.make(high_zoom_polygon_source, line_polygon_builder)
         features.make(high_zoom_line_source, line_polygon_builder)
         features.make(high_zoom_point_source, point_builder)
         
-      elsif index.z > 9
+      elsif index.z >= 9
         
         features.make(low_zoom_coastline_source, coastline_builder)
         features.make(medium_zoom_polygon_source, line_polygon_builder)

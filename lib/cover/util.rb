5b48e528d5b22a395fbf2915ce47b7a3eb69cebe
Michael Daines <michael@mdaines.com>
2012-08-14 14:29:03 -0500

    clean up scripts, add load_config and load_tile_indices to Util

--- lib/cover/util.rb
+++ lib/cover/util.rb
@@ -19,6 +19,41 @@ module Cover
         raise ArgumentError, "invalid zoom specifier"
       end
     end
+    
+    def self.load_config(path)
+      unless File.exist?(path)
+        raise ArgumentError, "Configuration file not found: #{path}"
+      end
+      
+      Cover.config = nil
+      require File.expand_path(path)
+      
+      unless Cover.config != nil
+        raise ArgumentError, "Configuration file did not assign Cover.config"
+      end
+      
+      unless Cover.config.respond_to?(:maker)
+        raise ArgumentError, "Object assigned to Cover.config does not respond to #maker"
+      end
+      
+      Cover.config
+    end
+    
+    def self.load_tile_indices(strings)
+      
+      strings.inject([]) do |indices, string|
+        if File.exist?(string)
+          File.open(string, "r").each_line do |line|
+            next if line =~ /^#/ || line =~ /^\s*$/
+            indices << Cover::TileIndex.new(line)
+          end
+        else
+          indices << Cover::TileIndex.new(string)
+        end
+        indices
+      end
+      
+    end
   
   end
   

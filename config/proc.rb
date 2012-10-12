7f0e5bd3b2dd06a71949fbf1387f373b8fc3fff7
Michael Daines <michael@mdaines.com>
2012-10-12 17:37:02 -0500

    different id for proc config feature

--- config/proc.rb
+++ config/proc.rb
@@ -1,5 +1,5 @@
 scale 256
 
 source :proc do |index|
-  [ { "id" => "123", "geometry" => { "type" => "Point", "coordinates" => [128, 128] } } ]
+  [ { "id" => "1", "geometry" => "{\"type\":\"Point\",\"coordinates\":[128,128]}" } ]
 end

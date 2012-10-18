afeb3a893abaa9f3bfca6a27c47ed54b8c9aab6f
Michael Daines <michael@mdaines.com>
2012-10-18 12:43:10 -0500

    output scale member only for :tile coordinates

--- lib/cover/tileset.rb
+++ lib/cover/tileset.rb
@@ -52,9 +52,9 @@ module Cover
       end
       
       io << "{"
-      io << "\"scale\":#{scale},"
       
       if coordinates == :tile
+        io << "\"scale\":#{scale},"
         io << "\"crs\":null,"
       end
       

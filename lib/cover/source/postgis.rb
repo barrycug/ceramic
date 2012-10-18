502f1099de0a8a405fa7175a9e684083834ebfc8
Michael Daines <michael@mdaines.com>
2012-10-18 13:03:53 -0500

    typecast hstore columns

--- lib/cover/source/postgis.rb
+++ lib/cover/source/postgis.rb
@@ -217,8 +217,51 @@ SQL
           case type
           when "integer" then value.to_i
           when "real"    then value.to_f
+          when "hstore"  then parse_hstore(value)
           else value
           end
+        end
+        
+        # Adapted from http://engageis.github.com/activerecord-postgres-hstore/
+        #
+        # Copyright (c) 2009 Juan Maiz
+        # 
+        # Permission is hereby granted, free of charge, to any person obtaining
+        # a copy of this software and associated documentation files (the
+        # "Software"), to deal in the Software without restriction, including
+        # without limitation the rights to use, copy, modify, merge, publish,
+        # distribute, sublicense, and/or sell copies of the Software, and to
+        # permit persons to whom the Software is furnished to do so, subject to
+        # the following conditions:
+        # 
+        # The above copyright notice and this permission notice shall be
+        # included in all copies or substantial portions of the Software.
+        # 
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+        # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+        # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+        # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        
+        HSTORE_QUOTED_STRING = /"[^"\\]*(?:\\.[^"\\]*)*"/
+        HSTORE_UNQUOTED_STRING = /[^\s=,][^\s=,\\]*(?:\\.[^\s=,\\]*|=[^,>])*/
+        HSTORE_STRING = /(#{HSTORE_QUOTED_STRING}|#{HSTORE_UNQUOTED_STRING})/
+        HSTORE_PAIR = /#{HSTORE_STRING}\s*=>\s*#{HSTORE_STRING}/
+        
+        def parse_hstore(string)
+          token_pairs = (string.scan(HSTORE_PAIR)).map { |k,v| [k,v =~ /^NULL$/i ? nil : v] }
+          token_pairs = token_pairs.map { |k,v|
+            [k,v].map { |t|
+              case t
+              when nil then t
+              when /\A"(.*)"\Z/m then $1.gsub(/\\(.)/, '\1')
+              else t.gsub(/\\(.)/, '\1')
+              end
+            }
+          }
+          Hash[token_pairs]
         end
       
     end
